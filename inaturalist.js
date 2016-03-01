ed6018707d79c9ae9b83839692eba8c1a9cb47d5
Calvin Dong <calvinldong@gmail.com>
2016-02-29 23:11:08 -0800

    got response headers

--- inaturalist.js
+++ inaturalist.js
@@ -1,26 +1,49 @@
 var cleaned=[];
-for(a=0;a<100;a++){
 
 
-$.ajax({
-	url: "http://inaturalist.org/observations.json?q='Puma concolor'&per_page=200&page="+a
+var jqXHR=$.ajax({
+	url: "http://inaturalist.org/observations.json?q='Puma concolor'&per_page=200&page=1"
 
 
 }
 
 	).done(function(data){
-		console.log(data);
+    var headers = jqXHR.getAllResponseHeaders();
+    console.log(headers);
+    console.log(data);
+	//console.log(data);
 		for(b=0;b<data.length;b++){
 			if(data[b]!=null){
 				var better={'latitude': data[b].latitude , 'longitude': data[b].longitude};
-				console.log(better);
+				//console.log(better);
 				cleaned.push(better);
 				
 			}
 		}
-		console.log(cleaned);
+		//console.log(cleaned);
 });
 
 
+
+$.ajax({
+	url: "http://inaturalist.org/observations.json?q='Puma concolor'&per_page=200&page=2"
+
+
 }
+
+	).done(function(data,request){
+    
+    
+	//console.log(data);
+		for(b=0;b<data.length;b++){
+			if(data[b]!=null){
+				var better={'latitude': data[b].latitude , 'longitude': data[b].longitude};
+				//console.log(better);
+				cleaned.push(better);
+				
+			}
+		}
+		//console.log(cleaned);
+});
+
 
