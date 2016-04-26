95ff02d5fee0dd97c3747caf99b45f32ee8c3e31
Jonathan Wang <jonathanwang017@berkeley.edu>
2016-04-26 01:59:28 -0700

    add image search

--- js/api/helper.js
+++ js/api/helper.js
@@ -10,6 +10,24 @@ function getCountryCode(countryName) {
   }
 }
 
+function search_img(query, api_dfd, img_result) {
+  $.ajax({
+    type: 'POST',
+    dataType: 'jsonp',    
+    url: 'https://www.googleapis.com/customsearch/v1?q=' + query + '&key=AIzaSyB9GKaKX2TRnMH8M28bVD8BfG-rR7H7RJs&cx=006284769470110551168:rbgx9geslba',
+  }).done(function(data) {
+    // check if there are results
+    if (data.length != 0) {
+      img_result['img'] = data.items[0].pagemap.cse_image[0].src;
+    } else {
+      img_result['img'] = null;
+    }
+
+    // notify search complete
+    api_dfd.resolve();
+  });
+}
+
 
 var isoCountries = {
   'Canada' : 'CA',
--- js/client/controller.js
+++ js/client/controller.js
@@ -4,17 +4,16 @@ var databases = {
                 'idigbio': {'basic': search_idigbio, 'location': search_idigbio_location},
                 'gbif': {'basic': search_gbif, 'location': search_gbif_location},
                 'iucn': {'basic': search_iucn, 'location': search_iucn_location},
-                'inaturalist': {'basic': search_inat, 'location': search_inat},
+                'inaturalist': {'basic': search_inat, 'location': search_inat_location},
                 'nas': {'basic':search_nas, 'location': search_nas_location}
                 };
 
-searchDatabase = function(query, location, search_dfd, results, species_info) {
+searchDatabase = function(query, location, search_dfd, results, img_result, species_info) {
   var all_dfd = [];
 
   // search each database
   $.each(databases, function(db, func) {
-    // search complete notifier
-    var api_dfd = $.Deferred();
+    var api_dfd = $.Deferred()
 
     // add deferred to list of deferreds
     all_dfd.push(api_dfd);
@@ -28,9 +27,14 @@ searchDatabase = function(query, location, search_dfd, results, species_info) {
   });
 
   // search for common name and taxonomy
-  var api_dfd = $.Deferred();
+  var api_dfd = $.Deferred()
   all_dfd.push(api_dfd);
   get_species_info(query, api_dfd, species_info);
+
+  // search for image
+  var api_dfd = $.Deferred()
+  all_dfd.push(api_dfd);
+  search_img(query, api_dfd, img_result);
 
   // return results after all searches complete
   $.when.apply(this, all_dfd).done(function() {
@@ -44,6 +48,7 @@ searchDatabase = function(query, location, search_dfd, results, species_info) {
 app.controller('searchController', function($scope) {
   // data model for results table
   $scope.search_result = {};
+  $scope.img = null;
 
   $scope.search = function(query, location){
     // all search complete notifier
@@ -53,14 +58,16 @@ app.controller('searchController', function($scope) {
     // container object to modify
     var results = {};
     var species_info = {};
+    var img_result = {};
 
     $scope.query = query;
     $scope.taxonomy = 'Loading...';
     $scope.common_name = 'Loading...';
+    $scope.img = '';
     $scope.search_result = {'loading': {'database': 'Loading...', 'count': 'Loading...'}};
 
     // start database searches
-    searchDatabase(query, location, search_dfd, results, species_info);
+    searchDatabase(query, location, search_dfd, results, img_result, species_info);
 
     // return when all searches are complete
     search_dfd.done(function() {
@@ -69,6 +76,7 @@ app.controller('searchController', function($scope) {
         $scope.search_result = results;
         $scope.taxonomy = species_info['taxonomy'];
         $scope.common_name = species_info['common_name'];
+        $scope.img = img_result['img'];
       });
     });
   }
