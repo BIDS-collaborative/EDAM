480d4ed0bf3c2df4050319aa092e96fa3a30d2dd
linbrian <linbrian@berkeley.edu>
2016-10-11 15:18:22 -0700

    Brian: Removed evaluation from data. 0 is now low-risk while 1 is high risk

--- brian/makeytrain.R
+++ brian/makeytrain.R
@@ -7,19 +7,28 @@ df$Scientific.Name = tolower(df$Scientific.Name)
 recommendations = gsub("[[:punct:]]", "", df$Recommendation)
 
 risks = c()
+evaluates = c()
 for (i in 1:length(df2[[1]])) {
   index = which(df$Scientific.Name == df2[[1]][i])[1]
-  recommendation = df$Recommendation[index]
+  recommendation = recommendations[index]
   if (recommendation == "low risk") {
     risks = c(risks, 0)
   }
   else if (recommendation == "evaluate") {
-    risks = c(risks, 1)
+    evaluates = c(evaluates, df2[[1]][i])
   }
   else {
-    risks = c(risks, 2)
+    risks = c(risks, 1)
   }
 }
+remove = c()
+for (i in 1:length(evaluates)) {
+  index = which(df2[[1]] == evaluates[i])
+  remove = c(remove, index)
+}
+df3 = df2[-remove, ]
+
 
 risks = data.frame(risks)
-write.csv(risks, file = "pier_html_labels.csv", row.names = FALSE)
+write.csv(df3, file = "pier_html_data_noevaluates.csv", row.names = FALSE)
+write.csv(risks, file = "pier_html_labels_noevaluates.csv", row.names = FALSE)
