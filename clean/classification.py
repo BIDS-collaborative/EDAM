d39a654969e5ffff456b2ae3bf0d65e5dfb00e6a
Jonathan Wang <jonathanwang017@berkeley.edu>
2016-12-05 19:00:23 -0800

    finish classification

--- clean/classification.py
+++ clean/classification.py
@@ -1,26 +1,109 @@
-# remove features missing data (detrimental features)
-def clean_features():
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
+# examine missing data by samples
+def explore_samples(data, threshold=5):
+  count = 0
+  rows = []
+  for i in range(len(data)):
+    missing_vals = np.sum(np.isnan(data[i]))
+    if missing_vals > threshold:
+      rows.append(i)
+      count += 1
+  print count
+  return rows
+
+# examine missing data by features
+def explore_features(data, threshold=100):
+  cols = []
+  for i in range(data.shape[1]):
+    missing_vals = np.sum(np.isnan(data[:, i]))
+    if missing_vals > threshold:
+      print i, missing_vals
+      cols.append(i)
+  return cols
+
+# Some statistics:
+# total 1427 plants
+# 1425 missing at least one feature
+# 632 (44%) missing more than 10 features
+# 196 (14%) missing more than 15 features
+# only 22 (2%) missing more than 20 features
+# all features missing in at least one sample
+
+
+# remove missing data (detrimental features and samples)
+def clean_features(features, labels):
+  # remove features by threshold
+  remove_cols = explore_features(features, 100)
+  features = np.delete(features, remove_cols, axis=1)
+  print features.shape
+  print '---'
+  
+  # remove samples by threshold
+  remove_rows = explore_samples(features, 0)
+  features = np.delete(features, remove_rows, axis=0)
+  labels = np.delete(labels, remove_rows)
+  print features.shape, labels.shape
+  print '---'
+
+  # TODO: efficiently remove NaNs while keeping as much data as possibles
+  return features, labels
+ 
 
 # split training and test data
-def split_data():
-
+def split_data(features, labels):
+  train_features, test_features, train_labels, test_labels = sklearn.cross_validation.train_test_split(features, labels, test_size = 0.2)
+  return train_features, test_features, train_labels, test_labels
 
 # build RF model
-def predict_rf():
-
+def predict_rf(train_features, test_features, train_labels, test_labels):
+  model = RandomForestClassifier(n_estimators=100)
+  model.fit(train_features, train_labels)
+  print model.score(train_features, train_labels)
+  print model.score(test_features, test_labels)
 
 # build LR model
-def predict_lr():
+def predict_lr(train_features, test_features, train_labels, test_labels):
+  model = LogisticRegression()
+  model.fit(train_features, train_labels)
+  print model.score(train_features, train_labels)
+  print model.score(test_features, test_labels)
+
+# Notes:
+# RF training accuracy: ~86%
+# RF test accuracy: ~73%
+# LR training accuracy: ~77%
+# LR test accuracy: ~77%
+# RF is more overfit but potentially better prediction
 
 
-# select important features
-def get_feature_importance():
+# test PCA and RF feature importances
+def get_feature_importance(features):
+  model = PCA(n_components=18)
+  model.fit(features)
+  plt.bar(range(18), model.explained_variance_ratio_)
+  plt.show()
+
+  model = RandomForestClassifier(n_estimators=500)
+  model.fit(features, labels)
+  plt.bar(range(18), model.feature_importances_)
+  plt.show()
+
+# Notes:
+# PCA: 3 features contain 50% of variance 
+# RF: variance much more uniformly distributed
 
 
-# test PCA
-def pca():
 
-
-# manual feature selection
-def subset_feature():
+features = np.genfromtxt('pacific_plant_data.csv', delimiter=',')
+labels = np.genfromtxt('pacific_plant_label.csv')
+features, labels = clean_features(features, labels)
+# get_feature_importance(features)
+train_features, test_features, train_labels, test_labels = split_data(features, labels)
+predict_rf(train_features, test_features, train_labels, test_labels)
+predict_lr(train_features, test_features, train_labels, test_labels)
--- clean/create_data.py
+++ clean/create_data.py
@@ -1,9 +1,14 @@
-import requests, re, json, io, os, urllib
+import requests, re, json, io, os, urllib, csv
+import numpy as np
 from bs4 import BeautifulSoup
 from lxml import html
 
 VAL_LOOKUP = {'y': 1, 'n': 0, 'High': 2, 'Intermediate': 1, 'Low': 0}
+EDGE_CASES = [201, 202, 607]
 
+
+def parse_html(html):
+  return re.sub('[\s]+', ' ', re.sub('^[\s]+|[\s]+$|[\r\n]', '' , re.sub('<.*?>', '', html)))
 
 # scrape plant data
 def scrape_pacific_plants_names():
@@ -19,7 +24,7 @@ def scrape_pacific_plants_names():
   plant_scientific_names = []
   # clean parsed data
   for d in data:
-    plant_scientific_names.append(re.sub('[\s]+', ' ', re.sub('^[\s]+|[\s]+$|[\r\n]', '' , re.sub('<.*?>', '', str(d)))))
+    plant_scientific_names.append(parse_html(str(d)))
 
   # write names to json
   with open('plant_scientific_names.json', 'w') as datafile:
@@ -30,7 +35,7 @@ def scrape_pacific_plants_names():
   plant_common_names = []
   # clean parsed data
   for d in data:
-    plant_common_names.append(re.sub('[\s]+', ' ', re.sub('^[\s]+|[\s]+$|[\r\n]', '' , re.sub('<.*?>', '', str(d)))))
+    plant_common_names.append(parse_html(str(d)))
 
   # write names to json
   with open('plant_common_names.json', 'w') as datafile:
@@ -50,7 +55,7 @@ def scrape_pacific_plants_location():
   plant_location = []
   # clean parsed data
   for d in data:
-    plant_location.append(re.sub('[\s]+', ' ', re.sub('^[\s]+|[\s]+$|[\r\n]', '' , re.sub('<.*?>', '', str(d)))))
+    plant_location.append(parse_html(str(d)))
 
   # write locations to json
   with open('plant_locations.json', 'w') as datafile:
@@ -77,7 +82,59 @@ def scrape_pacific_plants_links():
 
 
 def scrape_pacific_plants_html(link):
-  return
+  if re.search('http', link) is None:
+    link = 'http://www.hear.org/pier/' + link
+
+  r = requests.get(link)
+  html = BeautifulSoup(r.text, 'lxml')
+
+  # check Australian table format
+  question_index = 0
+  answer_index = -1
+  aus = False
+  try:
+    table = html.find_all('table')[1]
+  except:
+    table = html.find_all('table')[0]
+    aus = True
+    question_index = 2
+
+  rows = table.find_all('tr')
+  header = rows[0]
+  rows = rows[1:-1]
+
+  # check for score column
+  if parse_html(str(header.find_all('td')[-1])) == 'Score':
+    answer_index -= 1
+
+  feature_dict = dict()
+  for row in rows:
+    data = row.find_all('td')
+    # end of Australian table
+    if aus and parse_html(str(data[3])) == 'Outcome:':
+      break
+
+    # check if feature row
+    try:
+      feature_id = int(round(float(parse_html(str(data[question_index]))) * 100))
+    except:
+      continue
+
+    answer = parse_html(str(data[answer_index])).lower()
+
+    if feature_id in EDGE_CASES:
+      try:
+        feature_dict[feature_id] = int(answer)
+      except:
+        feature_dict[feature_id] = 'NA'
+    else:
+      if answer in VAL_LOOKUP:
+        feature_dict[feature_id] = VAL_LOOKUP[answer]
+      else:
+        feature_dict[feature_id] = 'NA'
+    
+  return feature_dict
+
 
 def is_table_line(tokens):
   try: 
@@ -89,8 +146,11 @@ def is_table_line(tokens):
 def is_table_end(tokens):
   return tokens[0] == 'Designation:' or (tokens[0] == 'Total' and tokens[1] == 'Score')
 
-
+# need to fix florida (tncflwra)
 def scrape_pacific_plants_pdf(link):
+  if re.search('http', link) is None:
+    link = 'http://www.hear.org/pier/' + link
+
   # create temp file
   pdfname = link.split('/')[-1].replace('%20', '_')
   txtname = re.sub('.pdf', '.txt', pdfname)
@@ -104,6 +164,7 @@ def scrape_pacific_plants_pdf(link):
     urllib.urlretrieve(link, pdfname)
   except:
     print 'Failed to download: {}'.format(pdfname)
+    return
 
   # convert pdf to txt
   # requires installation of xpdf
@@ -112,80 +173,159 @@ def scrape_pacific_plants_pdf(link):
     os.system('rm {}'.format(pdfname))
   except:
     print 'Please make sure Xpdf is installed and pdfPath is correct'
+    return
 
-  # parse pdf content
+  # parse pdf content - extract answer to questions, not score value
   feature_dict = dict()
-  with io.open(txtname, encoding='ISO-8859-1') as pdffile:
-    for line in pdffile:
-      tokens = [t for t in re.split('\s+', line) if t]
-      if tokens:
-        if is_table_line(tokens):
+  # catch file open exception
+  try:
+    with io.open(txtname, encoding='ISO-8859-1') as pdffile:
+      for line in pdffile:
+        tokens = [t for t in re.split('\s+', line) if t]
+        if tokens:
+          if is_table_line(tokens):
 
-          # convert question to integer key
-          try:
-            feature_id = int(tokens[0])
-          except:
-            feature_id = int(round(float(tokens[0]) * 100))
+            # convert question to integer key
+            try:
+              feature_id = int(tokens[0])
+            except:
+              feature_id = int(round(float(tokens[0]) * 100))
 
-          # alternate pdf format
-          if alt:
-            # edge cases
-            if feature_id in [201, 202, 607]:
-              try:
-                feature_dict[feature_id] = int(tokens[-1])
-              except:
-                feature_dict[feature_id] = 'NA'
-            # lookup Y/N values
+            # alternate pdf format
+            if alt:
+              # edge cases
+              if feature_id in EDGE_CASES:
+                try:
+                  feature_dict[feature_id] = int(tokens[-1])
+                except:
+                  feature_dict[feature_id] = 'NA'
+              # lookup Y/N values
+              else:
+                if tokens[-1] in VAL_LOOKUP:
+                  feature_dict[feature_id] = VAL_LOOKUP[tokens[-1]]
+                elif tokens[-2] in VAL_LOOKUP:
+                  feature_dict[feature_id] = VAL_LOOKUP[tokens[-2]]
+                else:
+                  feature_dict[feature_id] = 'NA'
             else:
+              # lookup values
               if tokens[-1] in VAL_LOOKUP:
                 feature_dict[feature_id] = VAL_LOOKUP[tokens[-1]]
-              elif tokens[-2] in VAL_LOOKUP:
-                feature_dict[feature_id] = VAL_LOOKUP[tokens[-2]]
+              # edge numeric cases
               else:
-                feature_dict[feature_id] = 'NA'
-          else:
-            # lookup values
-            if tokens[-1] in VAL_LOOKUP:
-              feature_dict[feature_id] = VAL_LOOKUP[tokens[-1]]
-            # edge numeric cases
-            else:
-              try:
-                feature_dict[feature_id] = int(tokens[-1])
-              except:
-                feature_dict[feature_id] = 'NA'
-        
-        # end of questions
-        elif is_table_end(tokens):
-          break
+                try:
+                  feature_dict[feature_id] = int(tokens[-1])
+                except:
+                  feature_dict[feature_id] = 'NA'
+          
+          # end of questions
+          elif is_table_end(tokens):
+            break
 
-  # remove temp file
-  os.system('rm {}'.format(txtname))
+    # remove temp file
+    os.system('rm {}'.format(txtname))
+  except:
+    return feature_dict
+
   return feature_dict
+
+def write_to_csv(csv_name, data):
+  with open(csv_name, 'a') as csvfile:
+    csvfile.write(data.encode('utf-8').strip() + '\n')
 
 
 def scrape_pacific_plants_data():
+  # load link and name data files
   plant_links = json.load(open('plant_links.json'))
-  plant_data = []
-
-  for link in plant_links:
+  plant_names = json.load(open('plant_scientific_names.json'))
+  
+  # match name and scrape data from link
+  for link, name in zip(plant_links, plant_names):
+    plant_data = {}
+    print name
     if re.search('.*?.htm', link) is not None:
-      plant_data.append(scrape_pacific_plants_html(link))
+      plant_data = scrape_pacific_plants_html(link)
     elif re.search('.*?.pdf', link) is not None:
-      plant_data.append(scrape_pacific_plants_pdf(link))
+      plant_data = scrape_pacific_plants_pdf(link)
 
-# def join_pacific_plants():
+    # write data to csv
+    write_to_csv('plant_data.csv', name + ',' + ','.join([str(plant_data[k]) for k in sorted(plant_data.keys())]))
   
 
-# # subset pacific plants, remove no eval/no score
-# def clean_pacific_plants():
+# filter plants with missing data and by region (only Pacific)
+def clean_pacific_plants():
+  # load location data file
+  plant_locations = json.load(open('plant_locations.json'))
+
+  # check location and data completeness and write to file
+  with open('plant_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for row, location in zip(csvreader, plant_locations):
+      if location == 'Pacific' and len(row) == 50:
+        write_to_csv('pacific_plant_data.csv', ','.join(row).decode('utf-8'))
 
 
-# # read lists of invasive plants
-# def scrape_invasive_plants():
+# read lists of invasive plants
+def scrape_invasive_plants():
+  r = requests.get('http://www.invasiveplantatlas.org/distribution.html')
+  html = BeautifulSoup(r.text, 'lxml')
+
+  table = html.find('table')
+  rows = table.find_all('tr')[1:]
+
+  data = [row.find_all('td')[1] for row in rows]
+  invasive_plants = []
+
+  # parse html
+  for d in data:
+    invasive_plants.append(parse_html(str(d)))
+
+  # write invasive plants to json
+  with open('invasive_plants.json', 'w') as datafile:
+    json.dump(invasive_plants, datafile, indent=2, separators=(',', ':'))
 
 
-# # match plant data with invasive plants
-# def label_pacific_plants():
+# match plant data with invasive plants
+def label_pacific_plants():
+  # load invasive plants data file
+  invasive_plants = json.load(open('invasive_plants.json'))
+  species_dict = {}
 
-print scrape_pacific_plants_pdf('http://www.hear.org/pier/wra/pacific/Abelmoschus%20manihot.pdf')
-# print scrape_pacific_plants_pdf('http://www.hear.org/wra/tncflwra/pdfs/tncflwra_abrus_precatorius_ispm.pdf')
+  # convert invasive plants list to genus:species dictionary
+  for plant in invasive_plants:
+    tokens = plant.split(' ')
+    genus = str(tokens[0])
+    species = str(tokens[1])
+    # note: spp = all species of genus
+    if species == 'x':
+      species = tokens[2]
+    
+    if genus in species_dict.keys():
+      species_dict[genus].append(species)
+    else:
+      species_dict[genus] = [species]
+
+  # iterate through list of plants
+  with open('pacific_plant_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # check for match in invasive plants list
+    for row in csvreader:
+      tokens = row[0].split(' ')
+      genus = tokens[0]
+      if genus in species_dict.keys():
+        species = tokens[1]
+        if species == 'x':
+          species = tokens[2]
+
+        if species in species_dict[genus] or species_dict[genus] == 'spp.':
+          write_to_csv('pacific_plant_label.csv', '1')
+        else:
+          write_to_csv('pacific_plant_label.csv', '0')
+      else:
+        write_to_csv('pacific_plant_label.csv', '0')
+
+  
+# clean_pacific_plants()
+# scrape_pacific_plants_data()
+# scrape_invasive_plants()
+# label_pacific_plants()
