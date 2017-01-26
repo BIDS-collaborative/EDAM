80c93b7758108334a4ecfe7a2daf396e09909a81
Jonathan Wang <jonathanwang017@berkeley.edu>
2017-01-26 12:48:27 -0800

    add html/views starter code

--- app/analysis/views.py
+++ app/analysis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+  # load html from templates/
+  template = loader.get_template('analysis.html')
+  context = {
+
+  }
+  # respond with template with context
+  return HttpResponse(template.render(context, request))
--- app/webtool/views.py
+++ app/webtool/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+  # load html from templates/
+  template = loader.get_template('webtool.html')
+  context = {
+
+  }
+  # respond with template with context
+  return HttpResponse(template.render(context, request))
