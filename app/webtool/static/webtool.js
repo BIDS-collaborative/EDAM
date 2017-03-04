f8dcb73917c81b8c0c57453b7a13662764fe92f9
Justin Mi <justin.mi@berkeley.edu>
2017-03-04 00:16:10 -0800

    merging'

--- app/webtool/static/webtool.js
+++ app/webtool/static/webtool.js
@@ -1,29 +1,38 @@
+/* Shows given <div> and hides all other divs */ 
 function showPageElement(what)  
 {  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    obj.style.display = 'block';  
-    return false;  
-}  
-  
+  if (what == "0") {
+    hideAllElements(); 
+  }
+
+  hideAllElements();
+
+  // shows element
+  var obj = typeof what == 'object'  
+    ? what : document.getElementById(what);
+
+  obj.style.display = 'block';  
+  return false;  
+} 
+
+/* hides a given div */
 function hidePageElement(what)  
 {  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    obj.style.display = 'none';  
-    return false;  
+  var obj = typeof what == 'object'  
+    ? what : document.getElementById(what);  
+
+  obj.style.display = 'none';  
+  return false;  
 }  
-  
-function togglePageElementVisibility(what)  
-{  
-    var obj = typeof what == 'object'  
-        ? what : document.getElementById(what);  
-  
-    if (obj.style.display == 'none')  
-        obj.style.display = 'block';  
-    else  
-        obj.style.display = 'none';  
-    return false;  
-} 
+
+function hideAllElements() 
+{
+  var elements = new Array("random-forest", "logistic-regression", "clustering");
+  for (var i = elements.length - 1; i >= 0; i--) {
+      hidePageElement(document.getElementById(elements[i]));
+    }
+}
+
+$( document ).ready(function() {
+    /* make sure the javascript file doesn't do anything until the html is loaded */
+});
