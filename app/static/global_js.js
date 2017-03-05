9ee43ab6c6e951b6ed82c7ba6239a32d176a9ee3
Caleb Studuent <calebs11@berkeley.edu>
2017-03-05 13:16:44 -0800

    Customized tips for bar graph and matrix

--- app/static/global_js.js
+++ app/static/global_js.js
@@ -2,18 +2,25 @@ function createVerticalBarGraph(data, x, y) {
   var dimensions = margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = x - margin.left - margin.right,
     height = y - margin.top - margin.bottom;
+  var importance = d3.keys(data),
+  features = d3.values(data);
   var svg = d3.select('#feature_importance')
     .attr('width', x)
     .attr('height', y);
+  var pairs = [];
 
-  var barWidth = width/data.length;
-  var y = d3.scaleLinear().domain([0, d3.max(data)]).range([height, 0]);
-  var x = d3.scaleBand().domain(d3.range(0, data.length)).rangeRound([0, width]).padding(0.02);
+  for (i = 0; i < features.length; i++){
+    pairs.push([Math.round(importance[i]*1000)/1000, features[i]]);
+  }
+
+  var barWidth = width/importance.length;
+  var y = d3.scaleLinear().domain([0, d3.max(importance)]).range([height, 0]);
+  var x = d3.scaleBand().domain(d3.range(0, importance.length)).rangeRound([0, width]);
 
   var tip = d3.tip()
     .attr('class', 'd3-tip')
     .offset([-8, 0])
-    .html(function(d) { return 'Attribute Value: ' + d; });
+    .html(function(d) { return "Importance: " + d[0] + "<br/>" + "Feature: " + d[1]; });
 
   var g = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
@@ -21,23 +28,28 @@ function createVerticalBarGraph(data, x, y) {
     .attr('class', 'axis axis--x')
     .attr('transform', 'translate(0,' + height + ')')
     .call(d3.axisBottom(x));
+  var yAxis = d3.axisLeft(y);
+  yAxis.ticks(10);
+  yAxis.tickSizeOuter(0);
+  yAxis.tickPadding(3);
   g.append('g')
     .attr('class', 'axis axis--y')
-    .call(d3.axisLeft(y))
+    .call(yAxis);
 
   var bar = g.selectAll('.bar')
-    .data(data)
+    .data(pairs)
     .enter().append('g');
   bar.call(tip);
 
   bar.append('rect')
     .attr('class', 'd3rect')
-    .attr('y', function(d) { return y(d); })
-    .attr('x', function(d, i) { return i * barWidth; })
-    .attr('width', x.bandwidth())
-    .attr('height', function(d) { return height - y(d); })
+    .attr('y', function(d) { return y(d[0]); })
+    .attr('x', function(d, i) { return i * barWidth + 1; })
+    .attr('width', x.bandwidth()-1)
+    .attr('height', function(d) { return height - y(d[0]);})
     .on('mouseover', tip.show)
     .on('mouseout', tip.hide);
+
 }
 
 function createMatrix(data, x, y) {
