797e0e7cf8b8b22b35d42f67c7ce25be76b6435c
Caleb Studuent <calebs11@berkeley.edu>
2017-03-07 02:31:31 -0800

    Added scatter plot

--- app/analysis/static/analysis.js
+++ app/analysis/static/analysis.js
@@ -16,5 +16,18 @@ $(document).ready(function(){
       createMatrix(data, x, y);
     }
   });
+
+
+  // $.ajax({url: '/analysis/scatter_plot',
+  //   dataType: 'json',
+  //   success: function(data) {
+  //     var x = 800, y = 500;
+  //     createScatterPlot(data, x, y);
+  //   }
+  // });
+
+var testData = {"feature1": [1, 2, 5, 7, 9], "feature2": [2, 4, 7, 8, 11], "species": ["cactus", "rose", "tulip", "weed", "daisy"]};
+createScatterPlot(testData, 800, 500);
+
 });
 
