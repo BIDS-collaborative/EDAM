55cb6f330ccd6e315315b25994d230d9f01a02a2
linbrian <linbrian@berkeley.edu>
2017-03-24 17:25:05 -0700

    model selection should be working now

--- app/webtool/views.py
+++ app/webtool/views.py
@@ -15,6 +15,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import sklearn
+import itertools
+import os
+import json
+from sklearn.decomposition import PCA
+
 from .forms import DocumentForm 
 
 def index(request):
@@ -83,72 +89,6 @@ def split_data(features, labels):
   return train_features, test_features, train_labels, test_labels
 
 
-# examine missing data by samples
-def explore_samples(data, threshold=5):
-  count = 0
-  rows = []
-  for i in range(len(data)):
-    missing_vals = np.sum(np.isnan(data[i]))
-    if missing_vals > threshold:
-      rows.append(i)
-      count += 1
-  print(count)
-  return rows
-
-# examine missing data by features
-def explore_features(data, threshold=100):
-  cols = []
-  for i in range(data.shape[1]):
-    missing_vals = np.sum(np.isnan(data[:, i]))
-    if missing_vals > threshold:
-      print(i, missing_vals)
-      cols.append(i)
-
-
-def clean_features(features, labels):
-  # remove features missing in a lot of samples
-  feature_threshold = [300, 250, 200, 150, 100]
-  sample_threshold = [20, 15, 10, 5, 0]
-
-  for f, s in zip(feature_threshold, sample_threshold):
-    remove_cols = explore_features(features, f)
-    features = np.delete(features, remove_cols, axis=1)
-    print(features.shape)
-    
-    # remove samples missing data
-    remove_rows = explore_samples(features, s)
-    features = np.delete(features, remove_rows, axis=0)
-    labels = np.delete(labels, remove_rows)
-    print(features.shape, labels.shape)
-
-  # RATIONALE: any feature missing in more than 5% of 
-  # samples has no guarantee of being collected so do
-  # not include in model and any sample still missing
-  # data probably is fairly unknown or poorly recorded
-
-  # TODO: efficiently remove NaNs while keeping as much data as possibles
-  return features, labels
-
-
-def train_rf(train_features, train_labels):
-    print("train_rf")
-    model = RandomForestClassifier(n_estimators=1000)
-    print("rf model get")
-    print(np.shape(train_features), np.any(np.isnan(train_features)), np.all(np.isfinite(train_features)))
-    print(np.shape(train_labels), np.any(np.isnan(train_labels)), np.all(np.isfinite(train_labels)))
-
-    model.fit(train_features, train_labels)
-    print("rf model fitted")
-    return model
-    
-def train_lr(train_features, train_labels):
-    print("train_lr")
-    model = LogisticRegression()
-    model.fit(train_features, train_labels)
-    return model
-
-def get_accuracy(predictions, labels):
-    return float(np.sum(predictions == labels))/len(labels)
 
 
 def performClassification(data_features, data_label, model_name, train = False):
@@ -172,11 +112,145 @@ def performClassification(data_features, data_label, model_name, train = False):
     print(cm)
   return (model_name, predictions, cm)
 
+
+
+def load_data(data, labels):
+  BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+  directory =  BASE_DIR + "/documents/" 
+  print directory
+  print data
+  features = np.genfromtxt(directory + data, delimiter=',')
+  labels = np.genfromtxt(directory + labels, delimiter = ",")
+  # feature_names = np.genfromtxt(directory + static('pacific_plant_features.csv'), delimiter='\n', dtype=str)
+  return clean_features(features, labels)
+
+# examine missing data by samples
+def explore_samples(data, threshold=5):
+  count = 0
+  rows = []
+  for i in range(len(data)):
+    missing_vals = np.sum(np.isnan(data[i]))
+    if missing_vals > threshold:
+      rows.append(i)
+      count += 1
+  print (count)
+  return rows
+
+# examine missing data by features
+def explore_features(data, threshold=100):
+  cols = []
+  for i in range(data.shape[1]):
+    missing_vals = np.sum(np.isnan(data[:, i]))
+    if missing_vals > threshold:
+      print (i, missing_vals)
+      cols.append(i)
+  return cols
+
+# remove missing data (detrimental features and samples)
+def clean_features(features, labels):
+  # remove features missing in a lot of samples
+  feature_threshold = [300, 250, 200, 150, 100]
+  sample_threshold = [20, 15, 10, 5, 0]
+
+  for f, s in zip(feature_threshold, sample_threshold):
+    remove_cols = explore_features(features, f)
+    features = np.delete(features, remove_cols, axis=1)
+    # feature_names = np.delete(feature_names, remove_cols)
+    print (features.shape)
+    print ('---')
+
+    # remove samples missing data
+    remove_rows = explore_samples(features, s)
+    features = np.delete(features, remove_rows, axis=0)
+    labels = np.delete(labels, remove_rows)
+    print (features.shape, labels.shape)
+    print ('---')
+
+  # TODO: efficiently remove NaNs while keeping as much data as possibles
+  return features, labels
+
+# split training and test data
+def split_data(features, labels):
+  train_features, test_features, train_labels, test_labels = sklearn.model_selection.train_test_split(features, labels, test_size = 0.2)
+  return train_features, test_features, train_labels, test_labels
+
+def get_accuracy(predictions, labels):
+  return float(np.sum(predictions == labels))/len(labels)
+
+# build RF model
+def predict_rf(train_features, test_features, train_labels, test_labels):
+  model = RandomForestClassifier(n_estimators=1000)
+  model.fit(train_features, train_labels)
+  predictions = model.predict(train_features)
+  # print get_accuracy(predictions, train_labels)
+  predictions = model.predict(test_features)
+  # print get_accuracy(predictions, test_labels)
+  return predictions
+
+# build LR model
+def predict_lr(train_features, test_features, train_labels, test_labels):
+  model = LogisticRegression()
+  model.fit(train_features, train_labels)
+  predictions = model.predict(train_features)
+  # print get_accuracy(predictions, train_labels)
+  predictions = model.predict(test_features)
+  # print get_accuracy(predictions, test_labels)
+  return predictions
+
+def get_confusion_matrix(labels, predictions):
+  cm = np.array([[0, 0], [0, 0]])
+  for l, p in zip(labels.astype(int), predictions.astype(int)):
+    cm[l][p] += 1
+  norm_cm = cm.astype(float) / cm.sum(axis=1)[:, np.newaxis]
+  return np.round(norm_cm, 2), cm
+  # sklearn confusion matrix has encoding error
+  # cm = confusion_matrix(labels, predictions)
+  # print cm
+  # if normalize:
+  #   cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+  # return cm
+
+def get_feature_importance(features, labels):
+  model = RandomForestClassifier(n_estimators=1000)
+  model.fit(features, labels)
+  return model.feature_importances_
+
+def get_pca_variance(features):
+  model = PCA(n_components=18)
+  model.fit(features)
+  return model.explained_variance_ratio_
+
+def get_principal_components(features, n_features=18):
+  model = PCA(n_components=n_features)
+  model.fit(features)
+  return model.transform(features)
+
+#confusion matrix, feature importance, model predictions, pca variance
 @api_view(['GET'])
 def model_selection(request):
   model = request.query_params.get('model')
   hyperparameters = request.query_params.get('hyperparameters').split(',')
   features = request.query_params.get('features')
   labels = request.query_params.get('labels')
+  data = load_data(features, labels)
+  train_features, test_features, train_labels, test_labels = split_data(data[0], data[1])
+  if model == " LR":
+    predictions = predict_lr(train_features, test_features, train_labels, test_labels)
+  else:
+    predictions = predict_rf(train_features, test_features, train_labels, test_labels)
+  counts, cm = get_confusion_matrix(test_labels, predictions)
+  tips = [str(counts[0][0]) + ' out of ' + str(counts[0][0] + counts[0][1]),
+    str(counts[0][1]) + ' out of ' + str(counts[0][0] + counts[0][1]),
+    str(counts[1][0]) + ' out of ' + str(counts[1][0] + counts[1][1]),
+    str(counts[1][1]) + ' out of ' + str(counts[1][0] + counts[1][1])]
+  fi = get_feature_importance(data[0], data[1])
+  fi = fi.tolist()
+  pca = get_pca_variance(data[0])
 
-  return Response({"model": model, "hyperparameters": hyperparameters, "features": features, "labels": labels})
+
+  princomps = get_principal_components(data[0], 2)
+  feature1 = princomps[:,0]
+  feature2  = princomps[:,1]
+  species = [0]*len(princomps[:,0])
+  return Response({"feature_importance": {"features": np.zeros(len(fi)).tolist(), "importance": fi}, "predictions": predictions, "confusion_matrix": {"matrix": cm.tolist(), "tips": tips}, "pca": {"feature1": feature1, "feature2": feature2, "species": species, "label": data[1]}})
+  # return Response({"model": model, "hyperparameters": hyperparameters, "features": features, "labels": labels})
