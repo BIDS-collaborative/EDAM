534b20a125c6a9d63ae55e854d5be39974c0bec4
Justin Mi <JustinMi@users.noreply.github.com>
2017-06-13 23:58:46 -0700

    added OCF banner (#82)

--- app/analysis/urls.py
+++ app/analysis/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 app_name = 'analysis'
 urlpatterns = [
-  # route to app root
-  url(r'^$', views.index, name='index'),
+    # route to app root
+    url(r'^$', views.index, name='index'),
 
-  # route to each API
-  url(r'^confusion_matrix/', views.confusion_matrix),
-  url(r'^feature_importance/', views.feature_importance),
-  url(r'^pca_variance/', views.pca_variance),
-  url(r'^pca_scatter/', views.pca_scatter),
-  url(r'^pca_3d/', views.pca_3d),
+    # route to each API
+    url(r'^confusion_matrix/', views.confusion_matrix),
+    url(r'^feature_importance/', views.feature_importance),
+    url(r'^pca_variance/', views.pca_variance),
+    url(r'^pca_scatter/', views.pca_scatter),
+    url(r'^pca_3d/', views.pca_3d),
 ]
